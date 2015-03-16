40f615e876162b5e578e984de72667dfd22eac17
amithins <amith@cloudboxonline.com>
2015-03-17 02:17:28 +0530

    Update mactest.py

--- mactest.py
+++ mactest.py
@@ -1,7 +1,7 @@
 import base64
 import hmac
 import hashlib
-#comment
+
 shared_secret = "Kah942*$7sdp0)"
 plaintext= "10000GBP2007-10-20Internet Order 123454aD37dJATestMerchant2007-10-11T11:00:00Z"
 
